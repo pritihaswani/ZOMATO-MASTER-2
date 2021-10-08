741883eb0adea936cea3d724b0509947c6dcecda
Priti <pritihaswani1@gmail.com>
2021-10-09 00:20:46 +0530

    add

---
+++ server/API/Auth/index.js
@@ -0,0 +1,37 @@
+import express from "express";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+const Router = express.Router();
+
+//Models
+import { UserModel } from "../../database/user";
+
+/*
+Route          /signup
+Descrip        Signup with email and password
+Params         None
+Access         Public
+Method         POST
+*/
+
+Router.post("/signup", async(req,res) => {
+  try {
+    
+  await UserModel.findEmailAndPhone( req.body.credentials );
+
+
+//DB
+const newUser = await UserModel.create(req.body.credentials);
+
+//JWT Auth Token
+const token = newUser.generateJwtToken();
+
+return res.status(200).json({token});
+
+  } catch (error) {
+    return res.status(500).json({error: error.message});  
+  }
+});
+
+export default Router;
--- server/database/user/index.js
+++ server/database/user/index.js
@@ -1,4 +1,6 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 const UserSchema =new mongoose.Schema({
   fullname: {type: String, required: true},
@@ -9,6 +11,43 @@ const UserSchema =new mongoose.Schema({
 },
 {
   timestamps: true 
+});
+
+UserSchema.methods.generateJwtToken = function() {
+  return jwt.sign({user: this._id.toString()}, "ZomatoApp");
+};
+
+UserSchema.statics.findEmailAndPhone = async ({ email, phoneNumber }) => {
+  //check whether the email exists
+  const checkUserByEmail = await UserModel.findOne({email});
+
+  //check whether the phoneNumber exists
+  const checkUserByPhone = await UserModel.findOne({phoneNumber});
+  if(checkUserByEmail || checkUserByPhone) {
+    throw new Error( "User already Exists" );
+  }
+  return false;
+};
+
+UserSchema.pre("save",function(next){
+  const user = this;
+
+  //password is not modified
+  if(!user.isModified("password")) return next();
+
+  //generating bcrypt salt
+  bcrypt.genSalt(8,(error,salt)=> {
+    if(error) return next(error);
+
+    //hashing the password
+    bcrypt.hash(user.password, salt, (error,hash)=>{
+      if(error) return next(error);
+
+      //assigning hashed password
+      user.password = hash;
+      return next();
+    });
+  });
 });
 
 export const UserModel = mongoose.model("Users", UserSchema);
